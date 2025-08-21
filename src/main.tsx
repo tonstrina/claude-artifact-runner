c6d6e1f7319fe1d8cfe638cce8573ef3872f6735
tonstrina <paddycarr@gmail.com>
2025-08-21 14:52:32 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import routes from 'virtual:generated-pages-react';
-import Layout from './components/layout';
-import './index.css';
-
-const router = createBrowserRouter(
-  routes.map((route) => ({
-    ...route,
-    element: <Layout>{route.element}</Layout>,
-  })), {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true
-  }
-});
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ClientNotesApp from './artifacts'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ClientNotesApp />
   </React.StrictMode>,
-);
+)
