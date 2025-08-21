fdd321c6aba79e9e423231eb0cecb3e42bc71459
tonstrina <paddycarr@gmail.com>
2025-08-21 12:26:48 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// Fix the import path to point to your artifacts folder:
 import ClientNotesApp from './artifacts';
 
 const root = ReactDOM.createRoot(
