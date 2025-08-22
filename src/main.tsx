121f7f40f6f04a9a0a099a43e02f61a0249f4dfe
tonstrina <paddycarr@gmail.com>
2025-08-22 13:49:41 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ClientNotesApp from './artifacts'
-import './index.css'
+import '.src/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
